6a96d22eb594dc6aa48bddb55f9c05c3276e0317
s.felshtyn <s.felshtyn@tinkoff.ru>
2025-10-20 16:48:54 +0300

    add solution

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { createReadStream } from 'fs';
-import crypto from 'crypto';
-import http from 'http';
-import https from 'https';
-import pug from 'pug';
-import mongo from 'mongodb';
-import puppeteer from 'puppeteer-core';
+// index.js
+import express from "express";
+import bodyParser from "body-parser";
+import mongodb from "mongodb";
 
-import appSrc from './app.js';
+import appSrc from "./app.js";
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongo, https, pug, puppeteer);
+const app = appSrc(express, bodyParser, mongodb);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
